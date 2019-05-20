65888538af2e36af59f0c278d070be5fea6ed179
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2019-05-20 11:54:19 -0700

    default 100 results per page

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ import './App.css';
 // ];
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP='100';
 
 const PATH_BASE = 'http://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 // const isSearched = searchTerm => item =>
 //   item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -56,7 +58,7 @@ class App extends React.Component {
   }
 
   fetchSearchTopStories = (searchTerm, page = 0) => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
     .then(response => response.json())
     .then(result => this.setSearchTopStories(result))
     .catch(error => error);
